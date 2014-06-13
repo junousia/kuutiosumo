41e70c4f7f5973a8c9528aa319c7763d1ef349c7
Jukka Nousiainen <nousiaisenjukka@gmail.com>
2014-06-13 21:54:22 +0300

    Initial

---
+++ Assets/Scripts/ManagerScript.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+private var score = 0;
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function AddScore() {
+	score = score + 1;
+	var text : GUIText = transform.Find("Score").GetComponent(GUIText);
+	text.text = "" + score;
+	Debug.Log(score);
+	ResetPositions();
+}
+
+function ResetPositions() {
+	var vasen : PlayerScript = GameObject.Find("VasenPallo").GetComponent(PlayerScript);
+	vasen.ResetPosition();
+	
+	var oikea : PlayerScript = GameObject.Find("OikeaPallo").GetComponent(PlayerScript);
+	oikea.ResetPosition();
+}
---
+++ Assets/Scripts/PlayerScript.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+private var initialPosition : Vector3;
+public var score : GameObject;
+
+function Start () {
+	initialPosition = transform.position;
+}
+
+function Update () {
+}
+
+function OnBecameInvisible() {
+	var script : ManagerScript = score.GetComponent(ManagerScript);
+	script.AddScore();
+}
+
+function FixedUpdate () {
+}
+
+function Awake () {
+
+}
+
+function ResetPosition() {
+	transform.position = initialPosition;
+	rigidbody2D.velocity = new Vector2(Mathf.Sign(rigidbody2D.velocity.x) * 0, rigidbody2D.velocity.y * 0);
+}
